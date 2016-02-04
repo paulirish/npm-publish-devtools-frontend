d94eb6fa00b60f2e06c53f3c29def8202a964234
Paul Irish <paul.irish@gmail.com>
2016-02-04 00:44:40 -0800

    pull.sh added

--- publish.sh
+++ publish.sh
@@ -3,13 +3,21 @@
 publish_script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 frontend_path="$HOME/code/devtools-standalone"
 
-cp package.json $frontend_path
+cp $publish_script_path/package.json $frontend_path
 cd $frontend_path
 
 # get the chromium incremental commit position (e.g. 373466)
 commit_position=$(git log --no-color HEAD~1..HEAD | grep "Cr-Original-Commit-Position" | grep -E -o '#(\d+)' | grep -E -o '\d+')
 
-npm version "1.0.$commit_position"
-npm publish
+# verify we have a real number
+re='^[0-9]+$'
+if ! [[ $commit_position =~ $re ]] ; then
+   echo "error: Not a number" >&2; exit 1
+fi
 
+# bump and publish
+if npm version --no-git-tag-version "1.0.$commit_position"
+then npm publish
+fi
 
+rm ./package.json

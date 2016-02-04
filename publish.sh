b9c4e9df3be94e0a21c8a047c2efc046c72a7381
Paul Irish <paul.irish@gmail.com>
2016-02-04 00:29:05 -0800

    looking done

--- publish.sh
+++ publish.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
-publish_script_path="$PWD"
+publish_script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 frontend_path="$HOME/code/devtools-standalone"
 
 cp package.json $frontend_path
 cd $frontend_path
 
-~/code/devtools-standalone
+# get the chromium incremental commit position (e.g. 373466)
+commit_position=$(git log --no-color HEAD~1..HEAD | grep "Cr-Original-Commit-Position" | grep -E -o '#(\d+)' | grep -E -o '\d+')
+
+npm version "1.0.$commit_position"
+npm publish
+
+

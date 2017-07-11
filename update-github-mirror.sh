07b0e3fb2608512e1ff6a339510a61ad85345496
Paul Irish <paul.irish@gmail.com>
2017-07-11 14:19:24 -0700

    change local path.

--- update-github-mirror.sh
+++ update-github-mirror.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-frontend_path="$HOME/code/devtools-frontend-pristine"
+frontend_path="$HOME/code/pristine/devtools-frontend-pristine"
 
 # origin	https://chromium.googlesource.com/chromium/src/third_party/WebKit/Source/devtools
 # github	git@github.com:ChromeDevTools/devtools-frontend.git

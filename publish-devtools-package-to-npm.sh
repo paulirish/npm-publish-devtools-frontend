d5ba0030113ac8a8bff3bbbd5dee1eced104417d
Paul Irish <paul.irish@gmail.com>
2018-01-30 13:27:17 -0800

    include InspectorBackendCommands and SupportedCSSProperties

--- publish-devtools-package-to-npm.sh
+++ publish-devtools-package-to-npm.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
+set -x
+
 publish_script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-frontend_path="$HOME/code/devtools-frontend-pristine"
-
-cd $frontend_path
-
-# the gyp files cause big problems
-cp package.json package.json.bak.dontupload
+frontend_path="$HOME/code/pristine/devtools-frontend-pristine"
+chromium_src_path="$HOME/chromium-tot/src"
 
 
-# get the chromium incremental commit position (e.g. 373466)
-commit_position=$(git log --no-color HEAD~1..HEAD | grep "Cr-Original-Commit-Position" | grep -E -o '#(\d+)' | grep -E -o '\d+')
+cd $frontend_path || exit 1
+
+
+# get the chromium commit hash
+firstlog=$(git log --no-color HEAD~1..HEAD)
+commit_hash=$(printf "%s" "$firstlog" | grep "Cr-Mirrored-Commit" | grep -E -o ': (\w+)'| grep -E -o '(\w+)')
+# get the chromium rev (e.g. 373466)
+commit_position=$(printf "%s" "$firstlog" | grep "Cr-Original-Commit-Position" | grep -E -o '#(\d+)' | grep -E -o '\d+')
 
 # verify we have a real number
 re='^[0-9]+$'
@@ -18,14 +22,26 @@ if ! [[ $commit_position =~ $re ]] ; then
    echo "error: Not a number" >&2; exit 1
 fi
 
+
+# get the SupportedCSSProperties.js & InspectorBackendCommands.js files for this specific commit
+cd "$chromium_src_path" || exit 1
+git fetch origin master
+git checkout "$commit_hash"
+ninja -C "$chromium_src_path/out/Default/" supported_css_properties frontend_protocol_sources
+
+res_path="$chromium_src_path/out/Default/resources/inspector"
+cp "$res_path/InspectorBackendCommands.js" "$frontend_path"
+cp "$res_path/SupportedCSSProperties.js" "$frontend_path"
+cd $frontend_path || exit 1
+
+# The InspectorBackendCommands & SupportedCSSProperties files are not added to the git repo, as the github only has original sources.
+# They are only included in the published npm package
+
+
 # bump and publish
 if npm version --no-git-tag-version "1.0.$commit_position"
 then npm publish
 fi
 
-# clean up non-repo stuff
-mv package.json.bak.dontupload package.json
-
-# keeping these around won't conflict actually.
-# rm ./.npmignore
-# # rm ./npm-debug.log
+# revert the version bump in package.json
+git reset --hard

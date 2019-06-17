87c7c5c8eb75e80f3f05fbdd1ba047e410c40eec
Paul Irish <paul.irish@gmail.com>
2019-06-17 14:16:51 -0700

    exit 1 and also do ariaproperties.js

--- publish-devtools-package-to-npm.sh
+++ publish-devtools-package-to-npm.sh
@@ -28,11 +28,12 @@ cd "$chromium_src_path" || exit 1
 git fetch origin master
 git checkout "$commit_hash"
 GYP_DEFINES=disable_nacl=1 gclient sync --jobs=70 --nohooks
-ninja -C "$chromium_src_path/out/Default/" supported_css_properties frontend_protocol_sources
+ninja -C "$chromium_src_path/out/Default/" supported_css_properties frontend_protocol_sources aria_properties || exit 1
 
 res_path="$chromium_src_path/out/Default/resources/inspector"
-cp "$res_path/InspectorBackendCommands.js" "$frontend_path/front_end"
-cp "$res_path/SupportedCSSProperties.js" "$frontend_path/front_end"
+cp "$res_path/InspectorBackendCommands.js" "$frontend_path/front_end/" || exit 1
+cp "$res_path/SupportedCSSProperties.js" "$frontend_path/front_end/" || exit 1
+cp "$res_path/accessibility/ARIAProperties.js" "$frontend_path/front_end/accessibility/" || exit 1
 cd $frontend_path || exit 1
 
 # The InspectorBackendCommands & SupportedCSSProperties files are not added to the git repo, as the github only has original sources.

e5e63e1ec18601848c686da8ca17b339adb47259
Paul Irish <paul.irish@gmail.com>
2019-10-22 12:23:04 -0700

    update to standalone devtools dir

--- update-github-mirror.sh
+++ update-github-mirror.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+set -x
+
 frontend_path="$HOME/code/pristine/devtools-frontend-pristine"
 
-# origin	https://chromium.googlesource.com/chromium/src/third_party/blink/renderer/devtools/
-# github	git@github.com:ChromeDevTools/devtools-frontend.git
+# origin (deprecated)	https://chromium.googlesource.com/chromium/src/third_party/blink/renderer/devtools/
+# newstandalone			https://chromium.googlesource.com/devtools/devtools-frontend (fetch)
+# github				git@github.com:ChromeDevTools/devtools-frontend.git
 
-cd $frontend_path && git pull origin master && git push github master
+cd $frontend_path && git pull newstandalone master && git push github master
 

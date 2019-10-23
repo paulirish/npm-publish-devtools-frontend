54928193acc783b5b899eef269af6809aa7e3038
Paul Irish <paul.irish@gmail.com>
2019-10-22 17:45:01 -0700

    update for standalone repo move

--- publish-devtools-package-to-npm.sh
+++ publish-devtools-package-to-npm.sh
@@ -1,54 +1,71 @@
 #!/bin/bash
 
-set -x
+#####
+# find the commits that roll standalone devtools into chromium.
+# get the generated files from the roll commit
+# copy them back into standalone at the most recent commit of the roll range
+######
+
+set -ex -o pipefail
 
 publish_script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-frontend_path="$HOME/code/pristine/devtools-frontend-pristine"
+standalone_frontend_path="$HOME/code/pristine/devtools-frontend-pristine"
 chromium_src_path="$HOME/chromium-tot/src"
 
+cd "$chromium_src_path"
+git checkout -f origin/master # we assume the repo is recently updated
 
-cd $frontend_path || exit 1
+# find the most recent roll
+chromium_recent_roll_hash=$(git log --grep="third_party/devtools-frontend" -n1 --pretty="format:%H")
 
+# look at the roll range (of the standalone repo) and get the most recent commit of the two. 
+# use a lookbehind \K to only output what's after it. https://unix.stackexchange.com/a/13472
+standalone_commit_hash=$(git log $chromium_recent_roll_hash -n1 --pretty="format:%s" | ggrep --o -P '[a-f0-9]{7,}..\K([a-f0-9]{7,})')
 
-# get the chromium commit hash
-firstlog=$(git log --no-color HEAD~1..HEAD)
-commit_hash=$(printf "%s" "$firstlog" | grep "Cr-Mirrored-Commit" | grep -E -o ': (\w+)'| grep -E -o '(\w+)')
-# get the chromium rev (e.g. 373466)
-commit_position=$(printf "%s" "$firstlog" | grep "Cr-Original-Commit-Position" | grep -E -o '#(\d+)' | grep -E -o '\d+')
+# get the chromium rev (e.g. 373466) from the commit message body
+chromium_commit_position=$(git log $chromium_recent_roll_hash -n1 --pretty="format:%b"  | grep "Cr-Commit-Position" | grep -E -o '#(\d+)' | grep -E -o '\d+')
+
+chromium_commit_hash=$chromium_recent_roll_hash
+
 
 # verify we have a real number
 re='^[0-9]+$'
-if ! [[ $commit_position =~ $re ]] ; then
+if ! [[ $chromium_commit_position =~ $re ]] ; then
    echo "error: Not a number" >&2; exit 1
 fi
 
-
 # get the SupportedCSSProperties.js & InspectorBackendCommands.js files for this specific commit
-cd "$chromium_src_path" || exit 1
-git fetch origin master  || exit 1
-git checkout "$commit_hash"
-GYP_DEFINES=disable_nacl=1 gclient sync --delete_unversioned_trees --reset --jobs=70 || exit 1
-ninja -C "$chromium_src_path/out/Default/" supported_css_properties frontend_protocol_sources aria_properties || exit 1
+git checkout "$chromium_commit_hash"
+GYP_DEFINES=disable_nacl=1 gclient sync --delete_unversioned_trees --reset --jobs=70
+ninja -C "$chromium_src_path/out/Default/" supported_css_properties frontend_protocol_sources aria_properties
 
-res_path="$chromium_src_path/out/Default/resources/inspector"
-cp "$res_path/InspectorBackendCommands.js" "$frontend_path/front_end/" || exit 1
-cp "$res_path/SupportedCSSProperties.js" "$frontend_path/front_end/" || exit 1
-cp "$res_path/accessibility/ARIAProperties.js" "$frontend_path/front_end/accessibility/" || exit 1
+chromium_res_path="$chromium_src_path/out/Default/resources/inspector"
+cp "$chromium_res_path/InspectorBackendCommands.js" 		"$standalone_frontend_path/front_end/"
+cp "$chromium_res_path/SupportedCSSProperties.js" 			"$standalone_frontend_path/front_end/"
+cp "$chromium_res_path/accessibility/ARIAProperties.js" 	"$standalone_frontend_path/front_end/accessibility/"
 
-git checkout -f origin/master || exit 1
-cd $frontend_path || exit 1
 
-# The InspectorBackendCommands & SupportedCSSProperties files are not added to the git repo, as the github only has original sources.
-# They are only included in the published npm package
+################################
+# back to standalone repo      #
+################################
 
+cd $standalone_frontend_path
+git fetch origin master
+git checkout "$standalone_commit_hash"
+
+
+# The InspectorBackendCommands & SupportedCSSProperties files are only included in the published npm package, but wiped out afterwards.
 
 # bump and publish
-if npm version --no-git-tag-version "1.0.$commit_position"; then
+if npm version --no-git-tag-version "1.0.$chromium_commit_position"; then
   npm publish
 fi
 
 # revert the version bump in package.json
 git reset --hard
-# delete backend/css files to be extra safe
-git clean -f
+# delete backend/css files to be clean. (the -x is neccessary since these files are .gitignored these days)
+git clean -fdx
+
+# reset state
+git checkout origin/master
 
--- update-github-mirror.sh
+++ update-github-mirror.sh
@@ -4,9 +4,9 @@ set -x
 
 frontend_path="$HOME/code/pristine/devtools-frontend-pristine"
 
-# origin (deprecated)	https://chromium.googlesource.com/chromium/src/third_party/blink/renderer/devtools/
-# newstandalone			https://chromium.googlesource.com/devtools/devtools-frontend (fetch)
-# github				git@github.com:ChromeDevTools/devtools-frontend.git
+# old_blink (deprecated)	https://chromium.googlesource.com/chromium/src/third_party/blink/renderer/devtools/
+# origin					https://chromium.googlesource.com/devtools/devtools-frontend
+# github					git@github.com:ChromeDevTools/devtools-frontend.git
 
-cd $frontend_path && git pull newstandalone master && git push github master
+cd $frontend_path && git pull origin master && git push github master
 

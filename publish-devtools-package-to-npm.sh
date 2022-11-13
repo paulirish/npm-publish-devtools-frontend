2e50d99108de88bc00a3c0b945cade3707447b2e
Paul Irish <commits@paul.irish>
2022-11-13 12:50:54 -0800

    drop dependency on chromium-tot. simpler

--- publish-devtools-package-to-npm.sh
+++ publish-devtools-package-to-npm.sh
@@ -1,32 +1,21 @@
 #!/bin/bash
 
-#####
-# find the commits that roll standalone devtools into chromium.
-######
 
 set -ex -o pipefail
 
-publish_script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 standalone_frontend_path="$HOME/code/pristine/devtools-frontend-pristine"
-chromium_src_path="$HOME/chromium-tot/src"
+cd $standalone_frontend_path
 
-cd "$chromium_src_path"
 git fetch origin
 git checkout -f origin/main
+git reset --hard
+git clean -fdx
 
-# find the most recent roll
-chromium_recent_roll_hash=$(git log --grep=devtools -n1 --pretty="format:%H" -- DEPS)
-
-# look at the roll range (of the standalone repo) and get the most recent commit of the two.
-#     e.g.: "Roll DevTools Frontend from af6416cd74c8 to 038b9b33775b (1 revision)" 
-# use a lookbehind \K to only output what's after it. https://unix.stackexchange.com/a/13472
-standalone_commit_hash=$(git log $chromium_recent_roll_hash -n1 --pretty="format:%s" | ggrep --o -P 'from [a-f0-9]{7,} to \K([a-f0-9]{7,})')
-
-# get the chromium rev (e.g. 373466) from the commit message body
-chromium_commit_position=$(git log $chromium_recent_roll_hash -n1 --pretty="format:%b"  | grep "Cr-Commit-Position" | head -n1 | grep -E -o '#(\d+)' | grep -E -o '\d+')
-
-chromium_commit_hash=$chromium_recent_roll_hash
-
+# Find most recent roll of chromium INTO devtools-frontend standalone.
+#   NOTE: this isn't exactly the same as when frontend was rolled into chromium. But.. it shouldn't make a huge difference for these purposes.. :)
+#   A previous version of this script did the more involved lookup, but it involved a chromium checkout, which is a pain.
+# Specifically extract the chromim rev (e.g. 373466) from the DEPS file
+chromium_commit_position=$(cat DEPS | grep chromium_linux | head -n1 | grep -o -E "\d+")
 
 # verify we have a real number
 re='^[0-9]+$'
@@ -34,20 +23,10 @@ if ! [[ $chromium_commit_position =~ $re ]] ; then
    echo "error: Not a number" >&2; exit 1
 fi
 
-# UPDATE: the SupportedCSSProperties.js & InspectorBackendCommands.js files are now in front_end generated. (we used to have to build them, but now, no extra work required!)
 
-################################
-# back to standalone repo      #
-################################
-
-cd $standalone_frontend_path
-# revert the version bump in package.json
-git reset --hard
-# delete backend/css files to be clean. (the -x is neccessary since these files are .gitignored these days)
-git clean -fdx
 git fetch origin main
-git checkout "$standalone_commit_hash"
 
+echo "ready to publish: 1.0.$chromium_commit_position"
 
 # bump and publish
 if npm version --no-git-tag-version "1.0.$chromium_commit_position"; then
@@ -55,8 +34,6 @@ if npm version --no-git-tag-version "1.0.$chromium_commit_position"; then
 fi
 
 
-# and repeat so our pristine folder has no changes.
+# and repeat so our package.json has no version change
 git reset --hard
 git clean -fdx
-
-

a8b5072c769cc87792472202e75282bdf3c05085
Paul Irish <commits@paul.irish>
2024-10-02 12:57:09 -0700

    this broke june 2023 cuz no more rev in the DEPS. fixed now

--- publish-devtools-package-to-npm.sh
+++ publish-devtools-package-to-npm.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-set -ex -o pipefail
+set -euxo pipefail
 
 standalone_frontend_path="$HOME/code/pristine/devtools-frontend-pristine"
 cd $standalone_frontend_path
@@ -11,11 +11,14 @@ git checkout -f origin/main
 git reset --hard
 git clean -fdx
 
+
+# this is the chrome-for-testing version string, eg 131.0.6752.0
+chrome_version=$(cat DEPS | grep "'chrome'" | head -n1 | egrep -o "\d+\.\d+\.\d+\.\d+")
+
+
 # Find most recent roll of chromium INTO devtools-frontend standalone.
 #   NOTE: this isn't exactly the same as when frontend was rolled into chromium. But.. it shouldn't make a huge difference for these purposes.. :)
-#   A previous version of this script did the more involved lookup, but it involved a chromium checkout, which is a pain.
-# Specifically extract the chromim rev (e.g. 373466) from the DEPS file
-chromium_commit_position=$(cat DEPS | grep chromium_linux | head -n1 | grep -o -E "\d+")
+chromium_commit_position=$(curl "https://chromiumdash.appspot.com/fetch_releases?platform=Mac" | jq --arg chrome_version "$chrome_version" '.[] | select(.version == $chrome_version).chromium_main_branch_position')
 
 # verify we have a real number
 re='^[0-9]+$'
@@ -24,13 +27,27 @@ if ! [[ $chromium_commit_position =~ $re ]] ; then
 fi
 
 
+## TODO: at this point it'd be easier to publish with the chrome version but.. 
+# i dont see how to get the FULL chrome version string into an npm compatible version string, without problems or unexpected hyphens:
+#    ❯ npm version 130.0.3467.1
+#    v130.0.346-7.1
+#    
+#    ❯ npm version 1.130.0.3467.1
+#    npm ERR! Invalid version: 1.130.0.3467.1
+#    
+#    ❯ npm version 1.0.130.0.3467.1
+#    v1.0.13-0.0.3467.1
+#    
+#    ❯ npm version 1.0.0.130.0.3467.1
+#    npm ERR! Invalid version: 1.0.0.130.0.3467.1
+
 git fetch origin main
 
 echo "ready to publish: 1.0.$chromium_commit_position"
 
 # bump and publish
 if npm version --no-git-tag-version "1.0.$chromium_commit_position"; then
-  npm publish
+  npm publish --registry "https://registry.npmjs.org/"
 fi
 
 
